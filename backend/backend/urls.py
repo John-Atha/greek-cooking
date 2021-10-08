5c305e86c022e82de9651288427db6a5d83a9819
John-Atha <giannisj3@gmail.com>
2021-10-08 22:22:43 +0300

    Add recipe model, install Pillow, manage media

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main_app.urls")),
     path('api-auth/', include('rest_framework.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, 
+                         document_root=settings.MEDIA_ROOT)
--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.fields import EmailField
+from django.db.models import Count
 
 class User(AbstractUser):
     email = EmailField(max_length=50, unique=True, null=False)
 
     class Meta:
         ordering = ['id']
+
+class Recipe(models.Model):
+    title  = models.CharField(max_length=100, null=False)
+    description = models.TextField(max_length=5000, null=False)
+    image = models.ImageField(default=None, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes', null=False)
+    uploaded_at = models.DateTimeField()
+    fans = models.ManyToManyField(User, related_name='favourites', blank=True)
+
+    class Meta:
+        ordering = [Count('fans')]
